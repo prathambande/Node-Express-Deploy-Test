70e53cae928cb8d5d46c3e5b760f70fbaa3812ea
prathambande <81869581+prathambande@users.noreply.github.com>
2025-07-23 18:47:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     console.log('Hello endpoint called!');
-    res.send('Hello from NodeJs App Back!')
+    res.send('Hello from NodeJs App!')
 })
 
 app.get('/dummy', (req, res) => {
